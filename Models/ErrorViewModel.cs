8cc932515ab39ced176d592ec94bca26387a8949
Rakshitha Thanasekaran <trakshitha10@gmail.com>
2025-08-06 23:33:58 -0400

    Inital Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SafeVaultSecureApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
